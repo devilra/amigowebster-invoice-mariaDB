caf27192d308fd7485db90ecb9c172c16090c78d
kali raja <123291456+devilra@users.noreply.github.com>
2025-08-26 20:37:57 +0530

    Update api.js

--- frontent/src/api.js
+++ frontent/src/api.js
@@ -5,7 +5,7 @@ const basedURL= import.meta.env.VITE_BACKEND_URL
 console.log(basedURL)
 
 const API = axios.create({
-  baseURL:basedURL,
+  baseURL:'https://amigowebster-invoice-app.onrender.com',
   //baseURL: "http://localhost:4000",
 
   withCredentials: true,
