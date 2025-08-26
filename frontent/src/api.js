517db2b3f6210c72b1ed921ed4215393311a665c
devilra <rockerraja906@gmail.com>
2025-08-26 12:59:46 +0530

    change logo

--- frontent/src/api.js
+++ frontent/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: ' https://amigowebster-invoice-app.onrender.com',
   //baseURL: "http://localhost:4000",
   withCredentials: true,
 });
