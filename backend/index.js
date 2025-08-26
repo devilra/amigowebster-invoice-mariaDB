c22c33d562d85922990ba9334daf2dec8268d399
devilra <rockerraja906@gmail.com>
2025-08-26 13:13:36 +0530

    change logo

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://amigowebster-invoice-app.vercel.app",
-    //origin: "http://localhost:5173",
+    origin: ["https://amigowebster-invoice-app.vercel.app","http://localhost:5173" ],
+     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
