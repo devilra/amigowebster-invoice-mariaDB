b08f2805b55fe03b7f22efd78cb3acd7642f7abe
devilra <rockerraja906@gmail.com>
2025-09-08 11:51:51 +0530

    new db change

--- frontent/src/api.js
+++ frontent/src/api.js
@@ -5,7 +5,7 @@ const basedURL = import.meta.env.VITE_BACKEND_URL;
 //console.log(basedURL)
 
 const API = axios.create({
-  baseURL: "https://amigowebster-invoice-app.onrender.com",
+  baseURL: "https://amigowebster-invoice-mariadb.onrender.com",
   //baseURL: "http://localhost:4000",
 
   withCredentials: true,
